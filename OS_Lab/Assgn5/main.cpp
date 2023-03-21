db60e3e030cd700592c3400d2e7847952cd8db54
kgpian143 <vinodmeena.iitkgp@gmail.com>
2023-03-21 20:52:25 +0530

    os assgn 5

--- OS_Lab/Assgn5/main.cpp
+++ OS_Lab/Assgn5/main.cpp
@@ -10,11 +10,20 @@
 
 using namespace std;
 
+
+struct Guest
+{
+    int id ;
+    int rest_time ;
+    int priority ; 
+};
+
 struct Room
 {
     int occupancy_cnt;
     int waiting_time;
-    // int guest_priority;
+    Guest guest  ;
+    // int guest_id;
 };
 
 int n; // number of rooms
@@ -25,14 +34,14 @@ set<pair<int, int>> rooms;
 bool all_room_dirty = false;
 
 map<int, Room> room_details; // map for storing room details
-map<int, time_t> guest_entry_time;
+map<int, Guest> guest_details;
 
 
 sem_t booking_room_mutex;
 sem_t cleaning_room_mutex;
 
 pthread_mutex_t booking_lock ;
-pthread_mutex_t share_lock ; // lock for checking the all rooms are dirty or not 
+pthread_mutex_t clean_lock ; // lock for checking the all rooms are dirty or not 
 
 pthread_cond_t share_lock_cond ; 
 // int room_index
@@ -43,33 +52,44 @@ void *thread_cleaning(void *arg)
     cout << " Cleaning_Thread " << id << " : I am cleaning staff " << endl;
     while (1)
     {
-        sem_wait(&cleaning_room_mutex);
-        if (!all_room_dirty)
+        if(!all_room_dirty)
         {
-            cout << "Cleaning_Thread " << id << ": Waiting for the cleaning " << endl;
-            sleep(5) ;
-            sem_post(&cleaning_room_mutex) ;
-            continue;
+            sleep(1) ;
+            continue ;
         }
+
+        sem_wait(&cleaning_room_mutex);
 
         // Critical Section Start
 
         while (rooms.size() < n)
         {
+            // lock the mutex
+            pthread_mutex_lock(&clean_lock) ;
             int a = rand() % n + 1;
-            if (rooms.find({0, a}) == rooms.end())
+            bool room_cleaned = ( rooms.find({0, a}) != rooms.end() ) ;
+            pthread_mutex_unlock(&clean_lock) ;
+            if (!room_cleaned)
             {
+                if( room_details[a].guest.priority != 0 )
+                {
+                    guest_details[room_details[a].guest.id].rest_time = 0 ;
+                }
                 rooms.insert({0, a});
+                sleep(room_details[a].waiting_time / 3);
                 room_details[a].occupancy_cnt = 0;
                 room_details[a].waiting_time = 0;
-                sleep(room_details[a].waiting_time / 3);
                 cout << "Cleaning_Thread " << id << ": Cleaning staff cleaned the room " << a << endl;
-                if( rooms.size() == n)
+                
+                if( rooms.size() == n )
+                {
                     all_room_dirty = false;
+                }
+            }
+            else {
+                cout << "Cleaning_Thread " << id << ": Cleaning staff tried to clean the room " << a << " but it is already cleaned " << endl;
             }
         }
-        for( auto it : rooms )cout << it.second << " some message  " ;
-        cout << endl ; 
         sem_post(&cleaning_room_mutex);
 
         // Critical Section Ends
@@ -78,75 +98,92 @@ void *thread_cleaning(void *arg)
 
 void *thread_guest(void *arg)
 {
-    int guest_priority = *(int *)arg;
-    cout << "Guest_Thread " << guest_priority << " : I am guest thread " << endl;
+    int guest_id = *(int *)arg;
+    cout << "Guest_Thread " << guest_id << " : I am guest thread with priority : "<< guest_details[guest_id].priority << endl;
     while (1)
     {
-        cout <<"Guest_Thread " << guest_priority << " : " << rooms.size() << endl;
-        sleep(1) ;
-        while (all_room_dirty)
+        
+       // Critical Section Start
+        if( all_room_dirty )
         {
-            cout << "Guest_Thread " << guest_priority << ": All rooms are dirty, so I am waiting for cleaning staff " << endl;
-            // int sleep_time = rand() % 11 + 10;
-            sleep(3);
+            cout << "Guest_Thread " << guest_id << ": All rooms are dirty, so I am waiting for cleaning staff " << endl;
+            sleep(5) ;
             continue;
         }
+         
+        // pthread_mutex_lock(&share_lock);
 
-        // Critical Section Start
+        if (rooms.empty())
+        {
+            all_room_dirty = true;
+            continue;
+            // pthread_cond_signal(&share_lock_cond) ;
+        }
+
+        // pthread_mutex_unlock(&share_lock);
+
+
+        pthread_mutex_lock(&booking_lock);
+
+        auto it = rooms.begin();
+        if(it->first < guest_details[guest_id].priority )rooms.erase(it);
+
+        pthread_mutex_unlock(&booking_lock) ;
 
         sem_wait(&booking_room_mutex);
         
        // Lock the mutex
-        pthread_mutex_lock(&booking_lock);
-        if (rooms.empty())
-        {
-            all_room_dirty = true;
-            sem_post(&booking_room_mutex);
-            continue;
-        }
-        auto it = rooms.begin();
-        if (it->first < guest_priority)
+        if (it->first < guest_details[guest_id].priority )
         {
             // cout << 2 << endl ;
-            rooms.erase(it);
 
             time_t current_time = time(NULL);
-            guest_entry_time[guest_priority] = current_time;
             if (room_details[it->second].occupancy_cnt == 0)
             {
                 room_details[it->second].occupancy_cnt += 1;
-                rooms.insert({guest_priority, it->second});
-                pthread_mutex_unlock(&booking_lock);
-                cout << "Guest_Thread " << guest_priority << ": Guest got the room " << it->second << endl;
-                int rest_time = rand() % 21 + 10;
-                sleep(rest_time);
-                it = rooms.find({guest_priority, it->second});
+                room_details[it->second].guest.id = guest_id;
+                rooms.insert({guest_details[guest_id].priority, it->second});
+                cout << "Guest_Thread " << guest_id << ": Guest got the room " << it->second << endl;
+                guest_details[guest_id].rest_time = rand() % 21 + 10;
+                int rest_time = 0 ;
+                while(guest_details[guest_id].rest_time -- > 0  )
+                {
+                    sleep(1) ;
+                    rest_time ++ ;
+                    room_details[it->second].waiting_time += 1 ;
+                }
+                cout << "Guest_Thread " << guest_id << ": Guest stayed for " << rest_time << " days in room  " << it->second << endl;
+                it = rooms.find({guest_details[guest_id].priority, it->second});
                 if (it != rooms.end())
                 {
-                    cout << "Guest_Thread " << guest_priority << ": Guest stayed for " << rest_time << " days in room  " << it->second << endl;
-                    room_details[it->second].waiting_time += rest_time;
                     rooms.erase(it);
                     rooms.insert({0, it->second});
                 }
             }
             else
             {
-                pthread_mutex_unlock(&booking_lock);
                 if (it->first > 0)
                 {
-                    room_details[it->second].waiting_time = current_time - guest_entry_time[it->first];
-                    cout << "Guest_Thread " << guest_priority << ": Early Guest stayed for " << room_details[it->second].waiting_time << " in room  " << it->second << endl;
+                    guest_details[room_details[it->second].guest.id].rest_time = 0 ;
                 }
-                cout << "Guest_Thread " << guest_priority << ": Guest got the room " << it->second << endl;
-                int rest_time = rand() % 21 + 10;
-                sleep(rest_time);
-                cout << "Guest_Thread " << guest_priority << ": Guest stayed for " << rest_time << " in room  " << it->second << endl;
+                cout << "Guest_Thread " << guest_id << ": Guest got the room " << it->second << endl;
+                room_details[it->second].occupancy_cnt += 1;
+                room_details[it->second].guest = guest_details[guest_id] ;
+                guest_details[guest_id].rest_time = rand() % 21 + 10;
+                int rest_time = 0 ;
+                while(guest_details[guest_id].rest_time -- > 0  )
+                {
+                    sleep(1) ;
+                    rest_time ++ ;
+                    room_details[it->second].waiting_time += 1 ;
+                }
+                cout << "Guest_Thread " << guest_id << ": Guest stayed for " << rest_time << " days in room  " << it->second << endl;
             }
         }
         else 
         {
-            cout<<"Guest_Thread " << guest_priority << ": Not able to get room " << endl;
-            pthread_mutex_unlock(&booking_lock);
+            cout<<"Guest_Thread " << guest_id << ": Not able to get room " << endl;
+            // pthread_mutex_unlock(&booking_lock);
         }
         sem_post(&booking_room_mutex);
 
@@ -154,7 +191,7 @@ void *thread_guest(void *arg)
         int sleep_time = rand() % 11 + 10;
         sleep(sleep_time);
     }
-    cout << "Guest_Thread " << guest_priority << " : I am exiting " << endl;
+    cout << "Guest_Thread " << guest_id << " : I am exiting " << endl;
     pthread_exit(NULL) ;
 }
 int main()
@@ -174,6 +211,7 @@ int main()
 
     // mutext init
     pthread_mutex_init(&booking_lock, NULL); 
+    pthread_cond_init(&share_lock_cond , NULL) ;
     for (int i = 0; i < n; i++)
     {
         rooms.insert({0, i + 1});
@@ -185,18 +223,21 @@ int main()
 
     pthread_t cleaning_staff[x], guest[y];
 
+    for (int i = 0; i < y; i++)
+    {
+        int *id = new int;
+        *id = i + 1;
+        int priority = rand() % y + 1;
+        guest_details[i + 1].priority = priority;
+        pthread_create(&guest[i], NULL, thread_guest, id);
+        sleep(1) ;
+    }
+
     for (int i = 0; i < x; i++)
     {
         int *id = new int;
         *id = i + 1;
         pthread_create(&cleaning_staff[i], NULL, thread_cleaning, id);
-        sleep(1) ;
-    }
-    for (int i = 0; i < y; i++)
-    {
-        int *priority = new int;
-        *priority = i + 1;
-        pthread_create(&guest[i], NULL, thread_guest, priority);
         sleep(1) ;
     }
     for (int i = 0; i < x; i++)
